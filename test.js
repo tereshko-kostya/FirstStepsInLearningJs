bd454a7515d2163d1a62283ec87c3627b768cbab
kostya-tereshko <justlikecrab@yahoo.com>
2017-01-23 19:00:46 +0300

    alfaJavaSamec

--- test.js
+++ test.js
@@ -104,6 +104,34 @@ function myFunction(){
 	//========================================================================\\
 	//-----------------MATH-----------------\\
 	/*
-
+	*********METHODS*********
+		abs(x)	Returns the absolute value of x
+		acos(x)	Returns the arccosine of x, in radians
+		asin(x)	Returns the arcsine of x, in radians
+		atan(x)	Returns the arctangent of x as a numeric value between -PI/2 and PI/2 radians
+		atan2(y, x)	Returns the arctangent of the quotient of its arguments
+		ceil(x)	Returns the value of x rounded up to its nearest integer
+		cos(x)	Returns the cosine of x (x is in radians)
+		exp(x)	Returns the value of Ex
+		floor(x)	Returns the value of x rounded down to its nearest integer
+		log(x)	Returns the natural logarithm (base E) of x
+		max(x, y, z, ..., n)	Returns the number with the highest value
+		min(x, y, z, ..., n)	Returns the number with the lowest value
+		pow(x, y)	Returns the value of x to the power of y
+		random()	Returns a random number between 0 and 1
+		round(x)	Returns the value of x rounded to its nearest integer
+		sin(x)	Returns the sine of x (x is in radians)
+		sqrt(x)	Returns the square root of x
+		tan(x)	Returns the tangent of an angle
 	*/
+	//========================================================================\\
+	//-----------------RANDOM-----------------\\
+	/*
+	Math.random() - возвращает рандомное число.
+	По умолчанию, стандартный вызов метода, без аргументов, возвращает число типа float не больше 1.
+	Math.floor(Math.random() * 100) - вернёт рандомное целое число от 0 до 99.
+	Math.floor(Math.random() * 101) + 5 - вернёт рандомное целое число от 5 до 100.
+	*/
+	//========================================================================\\
+	//-----------------a-----------------\\
 }
